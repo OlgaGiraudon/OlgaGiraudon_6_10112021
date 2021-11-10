3a0f7305f18046dc706693fc988d1addeb708c9b
Olga Giraudon <giraudon.olga@gmail.com>
2021-11-10 13:49:46 +0100

    Put/Delete

--- app.js
+++ app.js
@@ -32,6 +32,18 @@ app.post('/api/sauces', (req, res, next) => {
       .then(() => res.status(201).json({ message: 'Votre sauce a été enregistrée !'}))
       .catch(error => res.status(400).json({ error }));
   });
+  //PUT API
+  app.put('/api/sauces/:id', (req, res, next) => {
+    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'La sauce a été modifiée !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+  //DELETE API
+  app.delete('/api/sauces/:id', (req, res, next) => {
+    Sauce.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
  // GET BY ID
  app.get('/api/sauces/:id', (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
